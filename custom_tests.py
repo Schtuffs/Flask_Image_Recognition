b0ca3475a1ce1be581e61a03e1c551bf2c863030
Schtuffs <152662980+Schtuffs@users.noreply.github.com>
2025-10-18 21:45:44 -0400

    basic test additions

---
+++ custom_tests.py
@@ -0,0 +1,22 @@
+"""
+These are the custom test cases for our assignment
+"""
+
+from app import app
+from model import preprocess_img
+from PIL import UnidentifiedImageError
+
+def test_empty_image():
+    """Basic: This tests an empty image"""
+    try:
+        preprocess_img('./test_images/0/empty.jpeg')
+        assert False
+
+    except UnidentifiedImageError:
+        assert True
+    
+def test_index_endpoint():
+    """Basic: Index route returns 200"""
+    assert app.test_client().get('/').status_code == 200
+
+def test_
