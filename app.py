63db0024a2d85d122fd4aed0d9eb76a2ab31ffab
Schtuffs <152662980+Schtuffs@users.noreply.github.com>
2025-10-18 23:08:21 -0400

    advanced test cases

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 
 
 # Home route
-@app.route("/")
+@app.route('/')
 def main():
     """Main"""
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 # Prediction route
@@ -26,20 +26,20 @@ def predict_image_file():
         if request.method == 'POST':
             img = preprocess_img(request.files['file'].stream)
             pred = predict_result(img)
-            return render_template("result.html", predictions=str(pred))
+            return render_template('result.html', predictions=str(pred))
 
     except UnidentifiedImageError:
-        error = "File cannot be processed."
-        return render_template("result.html", err=error)
+        error = 'File cannot be processed.'
+        return render_template('result.html', err=error)
     except FileNotFoundError:
-        error = "File cannot be processed."
-        return render_template("result.html", err=error)
+        error = 'File cannot be processed.'
+        return render_template('result.html', err=error)
     except BadRequestKeyError:
-        error = "File cannot be processed."
-        return render_template("result.html", err=error)
-    return render_template("result.html")
+        error = 'File cannot be processed.'
+        return render_template('result.html', err=error)
+    return render_template('result.html')
 
 
 # Driver code
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=9000, debug=True)
--- custom_tests.py
+++ custom_tests.py
@@ -2,19 +2,39 @@
 These are the custom test cases for our assignment
 """
 
+from werkzeug.exceptions import BadRequestKeyError
 from PIL import UnidentifiedImageError
-from app import app
-from model import preprocess_img
+from model import predict_result, preprocess_img
+
+# ----- Basic -----
+
+def test_index_endpoint(client):
+    """Basic: Index route returns 200"""
+    assert client.get('/').status_code == 200
+
+def test_predictions_post(client):
+    """Basic: Test predictions cannot be accessed through GET"""
+    assert client.get('/prediction').status_code == 405
+
+# ----- Advanced -----
 
 def test_empty_image():
-    """Basic: This tests an empty image"""
+    """Advanced: This tests an empty image"""
     try:
         preprocess_img('./test_images/0/empty.jpeg')
         assert False
-
     except UnidentifiedImageError:
         assert True
 
-def test_index_endpoint():
-    """Basic: Index route returns 200"""
-    assert app.test_client().get('/').status_code == 200
+def test_invalid_file():
+    """Advanced: Invalid file is properly disposed of"""
+    try:
+        predict_result(preprocess_img('fake_file.txt'))
+        assert False
+    except UnidentifiedImageError:
+        assert True
+    except FileNotFoundError:
+        assert True
+    except BadRequestKeyError:
+        assert True
+    assert True
